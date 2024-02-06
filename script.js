c68c264704c3b05849a9a468be4f46f4a732b6a3
verobelo <142001535+verobelo@users.noreply.github.com>
2024-02-06 20:18:40 +0100

    Add a function to create a grid of a desired size and be able to paint either in green or in random colors

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ function chooseRandomColor() {
   let b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+function createGrid() {
+  const gridSize = parseInt(gridInput.value);
+  gridContainer.innerHTML = "";
+  if (gridSize >= 3 && gridSize <= 25) {
+    document.documentElement.style.setProperty("--grid-size", gridSize);
+
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("grid-cell");
+        gridContainer.appendChild(gridCell);
+
+        function paintGreen() {
+          gridCell.addEventListener(
+            "mouseover",
+            () => (gridCell.style.backgroundColor = "#05f428")
+          );
+        }
+        btnGreen.addEventListener("click", paintGreen);
+
+        function paintRainbow() {
+          gridCell.addEventListener(
+            "mouseover",
+            () => (gridCell.style.backgroundColor = chooseRandomColor())
+          );
+        }
+        btnRainbow.addEventListener("click", paintRainbow);
+      }
+    }
+  }
+}
+
+createGrid();
