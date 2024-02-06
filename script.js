aa4f4444e95c34cd83a8406c03ecded0a2ed40dc
verobelo <142001535+verobelo@users.noreply.github.com>
2024-02-06 20:14:38 +0100

    Create variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const gridContainer = document.querySelector(".grid-container");
+const btnGreen = document.getElementById("green");
+const btnRainbow = document.getElementById("rainbow");
+const gridInput = document.getElementById("grid-input");
+const selectedInput = document.getElementById("selected-input");
