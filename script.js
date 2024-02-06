5795ae617faee3158c9267ea251b9986477c4d21
verobelo <142001535+verobelo@users.noreply.github.com>
2024-02-06 20:15:57 +0100

    Add event listener to input of the grid size

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const btnGreen = document.getElementById("green");
 const btnRainbow = document.getElementById("rainbow");
 const gridInput = document.getElementById("grid-input");
 const selectedInput = document.getElementById("selected-input");
+
+gridInput.addEventListener("input", () => {
+  selectedInput.textContent = gridInput.value;
+  createGrid();
+});
